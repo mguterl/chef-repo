112b623395b1564532357a63ef9db4ccb04227a9
Sidney Burks <sid137@gmail.com>
2009-08-15 06:47:42 +0200

    Capin

--- run-solo/solo.rb
+++ run-solo/solo.rb
@@ -1,3 +1,7 @@
-log_level :info
-file_cache_path "/srv/chef/"
-cookbook_path "/srv/chef/cookbooks"
+#
+# Example Chef Solo Config
+cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
+log_level         :info
+file_store_path  File.join(File.dirname(__FILE__), '..')
+file_cache_path  File.join(File.dirname(__FILE__), '..')
+Chef::Log::Formatter.show_time = false
