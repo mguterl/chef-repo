276fd3c75eb7f4ee610db9605ec510a428d1c6c5
Sidney Burks <sid137@gmail.com>
2009-08-15 02:19:40 +0200

    creating testing role

---
+++ roles/test.rb
@@ -0,0 +1,11 @@
+name "test"
+recipes "sudo"
+override_attributes "authorization" => {"sudo_users" => ["sid137"], "sudo_groups" => ["sid137"]}
+
+description 'Allows me to log in and test server config, functionality.  
+
+Creates a user "sid137"
+adds my public ssh key
+Adds sid137 to the sudoers file'
+
+
