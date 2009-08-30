cf5d38e12b4f1d00b712d3120be1df055bfd71b5
Sidney Burks <sid137@gmail.com>
2009-08-30 07:45:01 +0200

    added role_path to chef-solo config

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,7 +1,8 @@
 #
 # Example Chef Solo Config
-cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
 log_level         :info
-file_store_path  File.join(File.dirname(__FILE__), '..')
-file_cache_path  File.join(File.dirname(__FILE__), '..')
+file_store_path   File.join(File.dirname(__FILE__), '..')
+file_cache_path   File.join(File.dirname(__FILE__), '..')
+role_path         File.join(File.dirname(__FILE__), '..', "roles")
+cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
 Chef::Log::Formatter.show_time = false
