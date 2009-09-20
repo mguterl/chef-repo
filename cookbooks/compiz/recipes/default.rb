3e7775a114e2f5ad9382cd835e027f38b2ea8390
Sidney Burks <sid137@gmail.com>
2009-09-21 01:33:34 +0200

    addin compiz, firefox, vim

---
+++ cookbooks/compiz/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: compiz
+# Recipe:: default
+#
+# Copyright 2009, Sidney Burks
+#
+# All rights reserved - Do Not Redistribute
+
+package "compiz"
---
+++ cookbooks/vim/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: vim
+# Recipe:: default
+#
+# Copyright 2009, Sidney Burks
+#
+# All rights reserved - Do Not Redistribute
+#
+package "vim"
