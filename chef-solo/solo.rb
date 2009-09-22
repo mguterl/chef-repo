05db382c99760986297550cd88ed76f2d3c3c889
Sidney Burks <sid137@gmail.com>
2009-09-23 01:56:03 +0200

    CHEF-499 different prefix workaround

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,8 +1,11 @@
 #
 # Example Chef Solo Config
 log_level         :info
-file_store_path   File.join(File.dirname(__FILE__), '..')
-file_cache_path   File.join(File.dirname(__FILE__), '..')
+
+#commented because of CHEF-499
+#http://tickets.opscode.com/browse/CHEF-499
+#file_store_path   File.join(File.dirname(__FILE__), '..')
+#file_cache_path   File.join(File.dirname(__FILE__), '..')
 role_path         File.join(File.dirname(__FILE__), '..', "roles")
 cookbook_path     File.join(File.dirname(__FILE__), '..', "cookbooks")
 Chef::Log::Formatter.show_time = false
