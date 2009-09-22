049e5271fd8ac55ba03098731e5dd227fb4286a2
Sidney Burks <sid137@gmail.com>
2009-09-23 01:04:21 +0200

    made base recipe

---
+++ cookbooks/base/recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2009, Sidney Burks
+#
+# All rights reserved - Do Not Redistribute
+
+
+include_recipe "toolchain"
+include_recipe "zsh"
+
+packages = %w[
+  coreutils
+  git-core
+  screen
+  screen-profiles
+  ssl-cert
+  gzip
+  p7zip
+  p7zip-full
+  unzip
+  unrar
+  wget
+  curls
+  zlib1g
+  zlib1g-dev  
+
+  hfsplus
+  hfsprogs
+  hfsutils
+  ntfs3g
+  ntfsprogs
+  libntfs-3g-dev
+  reiserfsprogs
+]
+
+
+packages.each do |pkg|
+  package pkg do
+    action :upgrade
+  end
+end
+
+
---
+++ cookbooks/git/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2009, Sidney Burks
+#
+# All rights reserved - Do Not Redistribute
+
+
+
+
+
+package "git-core"
