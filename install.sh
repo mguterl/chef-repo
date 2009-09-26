3971baf586d5b6f1090937c53008d1fd9af3b721
Sidney Burks <sid137@gmail.com>
2009-09-27 01:59:34 +0200

    huh

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@
 role=${1:-test.json}
 
 apt-get install sudo curl git-core -y
-echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
-echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
-curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
-apt-get update
+#echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
+#echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
+#curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+#apt-get update
 
-aptitude install ohai chef rubygems -y
-git clone git://github.com/sid137/chef-repo.git /tmp/chef-repo
-cd /tmp/chef-repo/chef-solo/
-./solo $role.json
+#aptitude install ohai chef rubygems -y
+#git clone git://github.com/sid137/chef-repo.git /tmp/chef-repo
+#cd /tmp/chef-repo/chef-solo/
+#./solo $role.json
 
