8bda5929692175f607586ab6b7b22bd4e917b907
Sidney Burks <sid137@gmail.com>
2009-09-27 01:49:58 +0200

    tweaks

--- install.sh
+++ install.sh
@@ -6,8 +6,7 @@
 
 role=${1:-test.json}
 
-apt-get install sudo -y
-apt-get install curl git-core -y
+apt-get install sudo curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
