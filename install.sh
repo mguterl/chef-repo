9e6e657f9a51ac728e19900e1052274c6604d9ef
Sidney Burks <sid137@gmail.com>
2009-09-27 00:01:11 +0200

    installer work - apt repos

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-apt-get install curl
+apt-get install curl -y
 echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
+echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | sudo apt-key add -
 sudo apt-get update
 
