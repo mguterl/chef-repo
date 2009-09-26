9fb9aecdac836fc6166f68bf4b4a61086b0162fe
Sidney Burks <sid137@gmail.com>
2009-09-27 00:09:03 +0200

    installer work - removing sudo references

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-apt-get install sudo curl -y
+apt-get install sudo -y
+apt-get install curl -y
 echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
-curl http://apt.opscode.com/packages@opscode.com.gpg.key | sudo apt-key add -
-sudo apt-get update
+curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+apt-get update
 
 aptitude install ohai chef rubygems -y
