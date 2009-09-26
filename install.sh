73321f9b8045dba89432bca1885bb8bf7b58e5d4
Sidney Burks <sid137@gmail.com>
2009-09-27 00:10:32 +0200

    installer work - add git

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get install sudo -y
-apt-get install curl -y
+apt-get install curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
