ba87d1219b9230a648876b4a0af06f1ba990a98c
Sidney Burks <sid137@gmail.com>
2009-10-03 14:50:22 +0200

    upgrade output

--- cookbooks/upgrade/recipes/default.rb
+++ cookbooks/upgrade/recipes/default.rb
@@ -7,5 +7,5 @@
 # All rights reserved - Do Not Redistribute
 
 
-execute "apt-get dist-upgrade -f -y"
-execute "apt-get upgrade -f -y"
+execute "apt-get dist-upgrade -q -m f -y"
+execute "apt-get upgrade -q -m -f -y"
