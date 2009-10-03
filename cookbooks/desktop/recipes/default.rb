d9cd6131db4ccd1ca09423e2026baa92d741df91
Sidney Burks <sid137@gmail.com>
2009-10-03 17:09:09 +0200

    wicd config files

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -7,10 +7,12 @@
 # All rights reserved - Do Not Redistribute
 
 include_recipe "base"
+
 include_recipe "jungledisk"
 include_recipe "truecrypt"
 include_recipe "wine"
 include_recipe "compiz"
+include_recipe "wicd"
 
 packages = %w[
 gimp
@@ -18,7 +20,6 @@ gparted
 imagemagick
 ktorrent
 skype
-wicd
 ]
 
 
