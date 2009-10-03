26c5484daead490b5aae6dc0af17f28e91a1c1db
Sidney Burks <sid137@gmail.com>
2009-10-03 18:46:49 +0200

    installer

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
 #run installer with
-#wget http://github.com/sid137/chef-repo/raw/master/install.sh -O install.sh && /bin/bash -x install.sh role
+# wget http://tiny.cc/chefconfig; bash -x install.sh role 
 # role is a json file that describes a roles or runlist to execute.
 
 role=${1:-test.json}
 
 
-apt-get update
+apt-get update -q -m -y
 apt-get install locales sudo curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
-apt-get update
+apt-get update -q -m -y
 
 aptitude install ohai chef rubygems -y
 git clone git://github.com/sid137/chef-repo.git /tmp/chef-repo
