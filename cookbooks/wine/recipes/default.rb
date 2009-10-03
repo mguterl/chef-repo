6bf99a11e44d3185b593c40319a9f7e983bc8245
Sidney Burks <sid137@gmail.com>
2009-10-03 03:27:43 +0200

    wine fix

--- cookbooks/wine/recipes/default.rb
+++ cookbooks/wine/recipes/default.rb
@@ -9,7 +9,11 @@
 
 package "wine"
 
-directory "/home/sid137/.wine/drive_c/windows/system"
+directory "/home/sid137/.wine/drive_c/windows/system" do
+  owner "sid137"
+  group "sid137"
+  mode "0755"
+end
 
 remote_file "/home/sid137/.wine/drive_c/windows/system/MSVBVM60.DLL" do
   source "MSVBVM60.DLL"
