b7228005069f10f0f0767e3b2b2c54713cb65b9f
Sidney Burks <sid137@gmail.com>
2009-10-03 03:32:20 +0200

    wine

--- cookbooks/wine/recipes/default.rb
+++ cookbooks/wine/recipes/default.rb
@@ -9,11 +9,22 @@
 
 package "wine"
 
-directory "/home/sid137/.wine/drive_c/windows/system" do
-  owner "sid137"
-  group "sid137"
-  mode "0755"
+#directory "/home/sid137/.wine/drive_c/windows/system" do
+#  owner "sid137"
+#  group "sid137"
+#  mode "0755"
+#end
+script "wine_directory" do
+  interpreter "bash"
+  code <<-EOH
+  mkdir -p /home/sid137/.wine/drive_c/window/system
+  chown sid137 /home/sid137/.wine
+  chgrp sid137 /home/sid137/.wine
+
+  EOH
 end
+
+
 
 remote_file "/home/sid137/.wine/drive_c/windows/system/MSVBVM60.DLL" do
   source "MSVBVM60.DLL"
