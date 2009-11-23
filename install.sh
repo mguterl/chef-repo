722a4e016991968a5b11f024c355ec0afe42625a
sid137 <sid137@gmail.com>
2009-11-23 05:49:53 -0700

    Added Ubuntu Karmic repo

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ role=${1:-test.json}
 
 apt-get update -q -m -y
 apt-get install locales sudo curl git-core -y
-echo "deb http://apt.opscode.com/ jaunty universe" >/etc/apt/sources.list.d/opscode.list
-echo "deb http://apt.opscode.com/ debian contrib" >>/etc/apt/sources.list.d/opscode.list
+echo "deb http://apt.opscode.com/ jaunty universe" >  /etc/apt/sources.list.d/opscode.list
+echo "deb http://apt.opscode.com/ marmic universe" >> /etc/apt/sources.list.d/opscode.list
+echo "deb http://apt.opscode.com/ debian contrib"  >> /etc/apt/sources.list.d/opscode.list
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
 apt-get update -q -m -y
 
