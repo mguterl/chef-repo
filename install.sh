d4c08cd680260db9dd9c3aa36e2108fc1fe04cd6
Sidney Burks <sid137@gmail.com>
2010-02-17 00:48:37 +0100

    set locales from bash install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 role=${1:-test.json}
 
-
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen && /usr/sbin/locale-gen
 apt-get update -q -m -y
 apt-get install locales sudo curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >  /etc/apt/sources.list.d/opscode.list
