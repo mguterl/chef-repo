d30b7d6baa1a0ec49d555fe5bf30dfdbf47702aa
Sidney Burks <sid137@gmail.com>
2010-02-17 00:40:24 +0100

    tryingn locales fix

--- cookbooks/locales/recipes/default.rb
+++ cookbooks/locales/recipes/default.rb
@@ -8,10 +8,10 @@
 
 package "locales"
 
-template '/etc/locale.gen' do
-	source 'locale.gen.erb'
+remote_file '/etc/locale.gen' do
+	source 'locale.gen'
 end
 
-#execute "locale-gen en_US.UTF-8 UTF-8"
+execute "locale-gen en_US.UTF-8 UTF-8"
 #execute "/usr/sbin/update-locale LANG=en_US.UTF-8"
 
