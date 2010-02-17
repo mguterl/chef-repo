486317f3942dd38140ed5c7b01f13a666d80013b
Sidney Burks <sid137@gmail.com>
2010-02-17 02:24:48 +0100

    testing

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@
 role=${1:-test.json}
 
 # Configure locale settings
-apt-get install locales sudo -y
+apt-get install locales  -y
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen && /usr/sbin/locale-gen
 
-apt-get install curl git-core -y
+apt-get install sudo curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >  /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ karmic universe" >> /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib"  >> /etc/apt/sources.list.d/opscode.list
