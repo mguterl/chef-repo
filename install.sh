a80101998c60f52abea91cc86bc47eb39517da3b
Sidney Burks <sid137@gmail.com>
2010-02-17 02:45:30 +0100

    reogranization

--- install.sh
+++ install.sh
@@ -12,20 +12,24 @@ apt-get install locales  -y
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen && /usr/sbin/locale-gen
 
 apt-get install sudo curl git-core -y
+
+# Download server config
+git clone git://github.com/sid137/chef-repo.git /tmp/chef-repo
+cd /tmp/chef-repo/chef-solo/
+
+# Install Chef sources
 echo "deb http://apt.opscode.com/ jaunty universe" >  /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ karmic universe" >> /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib"  >> /etc/apt/sources.list.d/opscode.list
 apt-get update -q -m -y
 
 curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+
 # Install ruby, rubygems, and chef
 source ./scripts/install-rvm.sh
 aptitude install ohai chef -y
 
-
 # Run server ssetup
-git clone git://github.com/sid137/chef-repo.git /tmp/chef-repo
-cd /tmp/chef-repo/chef-solo/
 ./solo $role.json
 
 
