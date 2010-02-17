7183619eb749f4cd4af96b6c136abd285443701a
Sidney Burks <sid137@gmail.com>
2010-02-17 02:22:24 +0100

    testing

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ apt-get install curl git-core -y
 echo "deb http://apt.opscode.com/ jaunty universe" >  /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ karmic universe" >> /etc/apt/sources.list.d/opscode.list
 echo "deb http://apt.opscode.com/ debian contrib"  >> /etc/apt/sources.list.d/opscode.list
-curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
 apt-get update -q -m -y
 
+curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
 # Install ruby, rubygems, and chef
 source scripts/install-rvm.sh
 aptitude install ohai chef -y
