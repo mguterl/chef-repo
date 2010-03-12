c5d6696a90b9a604b20f606068dd13438ab57184
Sidney Burks <sid137@gmail.com>
2010-03-12 05:11:10 +0100

    installing mongodb-stqble

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2010, Sidney Burks
 #
 # All rights reserved - Do Not Redistribute
-#
+# http://www.linode.com/stackscripts/view/?StackScriptID=538
 
 
 case node[:platform] 
@@ -18,7 +18,7 @@ end
 
 execute "aptitude update -f -y"
 
-package "mongodb" do
+package "mongodb-stable" do
 	action :install
 	options "--force-yes"
 end
