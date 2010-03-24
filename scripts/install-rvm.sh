4ef0d076d402d5dc4848755949ac050cd1ad55dc
Sidney Burks <sid137@gmail.com>
2010-03-24 03:39:37 +0100

    rvm try again

--- scripts/install-rvm.sh
+++ scripts/install-rvm.sh
@@ -1,26 +1,20 @@
 #!/bin/bash -x
 
-# Create /etc/rvmrc
-#echo 'export rvm_path=/opt/rvm' > /etc/rvmrc
-
 
 # Install rvm
-#mkdir -p /opt/rvm/src/ 
-#cd /opt/rvm/src
-#rm -rf ./rvm/ 
-#git clone --depth 1 git://github.com/wayneeseguin/rvm.git
+mkdir -p /opt/rvm/src/ 
+cd /opt/rvm/src
+rm -rf ./rvm/ 
+git clone --depth 1 git://github.com/wayneeseguin/rvm.git
+./install --auto
 
+source /etc/profile
 
+#mkdir -p ~/.rvm/src/ && cd ~/.rvm/src && rm -rf ./rvm/ && git clone --depth 1
+#git://github.com/wayneeseguin/rvm.git && cd rvm && ./install --auto
 
-mkdir -p ~/.rvm/src/ && cd ~/.rvm/src && rm -rf ./rvm/ && git clone --depth 1
-git://github.com/wayneeseguin/rvm.git && cd rvm && ./install --auto
-
-
-# Configure System /etc/profile and/or /etc/bash.bashrc
-#echo 'if [[ -s /opt/rvm/scripts/rvm ]] ; then source /opt/rvm/scripts/rvm ;
-#fi' > /etc/profile
 
 # Install common rubies
-#source /opt/rvm/scripts/rvm
-rvm install ree  && rvm ree --default
+rvm install ree 
+rvm ree --default
 
