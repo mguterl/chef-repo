fe037f55120231028ce3e01a6f405f5b7d183dab
Sidney Burks <sid137@gmail.com>
2010-04-03 12:31:07 -0700

    changed to rubygems 1.3.6

--- scripts/install-rubygems.sh
+++ scripts/install-rubygems.sh
@@ -4,7 +4,7 @@ if gem -v
 then
   echo "Rubygems already installed"
 else
-  wget 'http://rubyforge.org/frs/download.php/60718/rubygems-1.3.5.tgz' -O /tmp/rubygems.tgz
+  wget 'http://rubyforge.org/frs/download.php/69365/rubygems-1.3.6.tgz' -O /tmp/rubygems.tgz
   cd /tmp && tar -zxvf rubygems.tgz
   cd /tmp/rubygems*
   ruby setup.rb
